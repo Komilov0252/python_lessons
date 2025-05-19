94ec6815ebb713f702feaec8eaa782e48185aa09
Komilov0252 <abrorbekkamilov571@gmail.com>
2025-05-19 19:28:35 +0500

    added spyder_1.py

---
+++ spyder_1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 15 19:03:23 2025
+
+@author: ironman
+"""
+from math import sqrt
+sonlar = list(range(11))
+ildizlar = list(map(sqrt, sonlar))
+print(sonlar)
+print(ildizlar)
